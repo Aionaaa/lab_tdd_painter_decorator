26df75b3e960cfe1b7063d03013bb54b5f933743
Iona Grant <ionagrant92@gmail.com>
2019-10-15 19:54:13 +0100

    started on decorator

--- decorator.js
+++ decorator.js
@@ -1,4 +1,8 @@
 const Decorator = function (name, paint) {
   this.name = name;
   this.paint = []
-}
+};
+
+Decorator.prototype.addPaint = function(paint) {
+  this.paint.push(paint)
+};
---
+++ specs/decorator_spec.js
@@ -0,0 +1,8 @@
+const assert = require("assert")
+const Decorator = require("../decorator.js");
+const Paint = require("../paint.js");
+const Room = require("../room.js");
+
+describe("Decorator", function(){
+  
+})
